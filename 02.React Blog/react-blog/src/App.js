ae5bade44c6a8eecb5943ea541ee2fa400673067
essay104 <alexis104@naver.com>
2024-06-25 23:16:13 +0900

    240625

---
+++ 02.React Blog/react-blog/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import New from './pages/New';
+import Diary from './pages/Diary';
+import Edit from './pages/Edit';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary/:id" element={<Diary />} />
+        <Route path="/edit/:id" element={<Edit />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
