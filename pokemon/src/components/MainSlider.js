f6c42de3de0208ae129dda39d9ec670c8df2c76b
essay104 <alexis104@naver.com>
2024-07-03 23:46:27 +0900

    240703

---
+++ pokemon/src/components/MainSlider.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./MainSlider.css"
+
+const MainSlider = () => {
+    return (
+        <div className='slider-wrap'>MainSlider</div>
+    )
+}
+
+export default MainSlider
--- pokemon/src/pages/Home.js
+++ pokemon/src/pages/Home.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import MainSlider from '../components/MainSlider'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <MainSlider />
+      <div className='Home-gnb'>
+        <div className='Home-gnb-menu'>포켓몬 정보</div>
+        <div className='Home-gnb-menu'>지역 정보</div>
+        <div className='Home-gnb-menu'>트레이너 정보</div>
+        <div className='Home-gnb-menu'>열매 정보</div>
+      </div>
+    </div>
   )
 }
 
